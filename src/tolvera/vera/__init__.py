5fc117b112646db98c749ecff535715544b5f205
G K <104482549+karegeo@users.noreply.github.com>
2025-04-06 16:21:02 +0900

    Update __init__.py

--- src/tolvera/vera/__init__.py
+++ src/tolvera/vera/__init__.py
@@ -7,6 +7,8 @@ from .slime import Slime
 from .particle_life import ParticleLife
 from .swarmalators import Swarmalators
 from .gol import GOL
+from .fractal import Fractal
+
 
 class Vera:
     """The Vera class provides a wrapper for all available forces and behaviours,
@@ -26,6 +28,7 @@ class Vera:
         self.plife = ParticleLife(tolvera, **kwargs)
         self.swarm = Swarmalators(tolvera, **kwargs)
         self.gol = GOL(tolvera, **kwargs)
+        self.fractal = Fractal(tolvera, **kwargs)
 
     def add_forces_to_self(self):
         """Add all forces to the Vera instance."""
